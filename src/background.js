699abf1669f75099f944e423feaa1c218ceeeab9
ncd2dq <ncd2dq@virginia.edu>
2019-04-26 15:20:01 -0400

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,4 +1,3 @@
-
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -26,6 +25,7 @@ chrome.runtime.onInstalled.addListener(function() {
       console.log("Blocker offline");
     });
 });
+
 
 let sweepInterval = 1000;
 let checkSweepStatusInterval = 10;
@@ -86,6 +86,7 @@ const sweepScript = `
       }
     }
   }
+  console.log("Sweeping...");
 })();
 `
 
